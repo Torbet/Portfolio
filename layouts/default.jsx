5e32e6693762580e0d284cc51b9756bb63434ba7
Guy Torbet <guy@guys-mbp.home>
2021-02-07 18:53:39 +0000

    added links to socials

--- layouts/default.jsx
+++ layouts/default.jsx
@@ -16,8 +16,6 @@ export default function Default(props) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta charSet="utf-8" />
                 <link rel="shortcut icon" href="/favicon.ico" />
-
-                <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/e539363b4b0f1208c8d23e3b7/062b6ffefded4a22c6462660a.js");</script>
             </Head>
             <div id={styles.main}>
                 <div id={styles.left}>
@@ -26,10 +24,10 @@ export default function Default(props) {
                         <p className={styles.subHeading}>I'm a 17 year old Software Developer and Student from Scotland.</p><br/>
                         <p className={styles.subHeading}>I read a lot online, and make a lot of projects; here I document and summarise the key ideas and insights from my experiences, and hopefully provide some value to you.</p>
                         <div id={styles.links}>
-                            <Link href="/"><FaGithub className={styles.icon} /></Link>
-                            <Link href="/"><FaTwitter className={styles.icon} /></Link>
-                            <Link href="/"><FaInstagram className={styles.icon} /></Link>
-                            <Link href="/"><FaInbox className={styles.icon} /></Link>
+                            <Link href="https://github.com/torbet"><FaGithub className={styles.icon} /></Link>
+                            <Link href="https://twitter.com/"><FaTwitter className={styles.icon} /></Link>
+                            <Link href="https://instagram.com/guytorbet"><FaInstagram className={styles.icon} /></Link>
+                            <Link href="mailto:guy@torbet.co"><FaInbox className={styles.icon} /></Link>
                         </div>
 
                     </div>
