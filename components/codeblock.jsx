5b5d6b306beb5888a9512bfa7ff9456a135f9452
Guy Torbet <guy@guys-mbp.home>
2021-02-14 02:46:26 +0000

    removed unused js

--- components/codeblock.jsx
+++ components/codeblock.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { atomOneDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import atomOneDark from 'react-syntax-highlighter/dist/cjs/styles/hljs/atom-one-dark';
 
 const CodeBlock = ({ language, value }) => {
 
