3c48fcaea35f426d4b1ddf6cd6f19fd05813511e
Guy Torbet <guy@guys-mbp.home>
2021-03-23 12:22:32 +0000

    schema

--- layouts/default.jsx
+++ layouts/default.jsx
@@ -20,6 +20,18 @@ export default function Default(props) {
                 <meta property="og:title" content="Guy Torbet | Portfolio" />
                 <meta property="og:description" content="A portfolio of Guy Torbet. Updating the progress of projects and summarising key information from what I read online - hopefully providing value." />
                 
+                <script type="application/ld+json">{`
+                    {
+                    "@context":"https://schema.org/",
+                    "@type":"Organization",
+                    "name":"Guy Torbet",
+                    "image":"https://torbet.co/favicon.ico"
+                    "logo":"https://torbet.co/favicon.ico"
+                    "url": "https://torbet.co"
+                    }
+                    `}
+                </script>
+                
             </Head>
             <div id={styles.main}>
                 <section id={styles.left}>
@@ -27,7 +39,7 @@ export default function Default(props) {
                         <h1 id={styles.title}>GUY TORBET.</h1><br />
                         <p className={styles.subHeading}>I'm a 17 year old Software Developer and Student from Scotland.</p><br/>
                         <p className={styles.subHeading}>I read a lot online, and make a lot of projects; here I document and summarise the key ideas and insights from my experiences, and hopefully provide some value to you.</p><br/>
-                        <p className={styles.subHeading}>I also have an email newsletter, it's about a 2-5 minute read every week. There's also a <a href="feed://torbet.co/feed.xml" className={styles.navItem}>RSS feed</a> if you're into that.</p>
+                        <p className={styles.subHeading}>I also have an email newsletter, it's about a 2-5 minute read every week. There's also a <a href="q/feed.xml" className={styles.navItem}>RSS feed</a> if you're into that.</p>
                         <Mail/>
                         <div id={styles.links}>
                             <Link href="https://github.com/torbet"><FaGithub className={styles.icon} /></Link>
