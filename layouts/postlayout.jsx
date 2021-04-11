9d26f73625032c5a79506c21b0b7dd5739c77eeb
Guy Torbet <guy@torbet.co>
2021-04-11 10:19:55 +0100

    didnt want player cards lol

--- layouts/postlayout.jsx
+++ layouts/postlayout.jsx
@@ -31,7 +31,7 @@ export default function PostLayout(props) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta charSet="utf-8" />
 
-                <meta name="twitter:card" content="player" />
+                <meta name="twitter:card" content="summary" />
                 <meta name="twitter:site" content="@guytorbet" />
                 <meta name="twitter:title" content={props.title} />
                 <meta name="twitter:description" content={props.description} />
