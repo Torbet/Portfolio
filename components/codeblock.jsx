1fff0bc7ad0d02d998fdaf161b8b09707f8cc2de
Guy Torbet <guy@torbet.co>
2021-04-12 10:52:38 +0100

    stuff

--- components/codeblock.jsx
+++ components/codeblock.jsx
@@ -4,21 +4,15 @@ import theme from 'react-syntax-highlighter/dist/cjs/styles/hljs/atom-one-dark';
 
 const CodeBlock = ({ language, value }) => {
 
-    const codeStyle = {
-        width: "100%"
-    }
-
     const blockStyle = {
-        fontFamily: 'Helvetica Neue',
         borderRadius: "20px",
         padding: '1em',
-        lineHeight: '1.5em',
         fontSize: "1.2em",
         marginBottom: '1em',
       }
 
     return (
-        <div style={codeStyle}>
+        <div>
             <SyntaxHighlighter language={language} style={theme} customStyle={blockStyle}>
                 {value}
             </SyntaxHighlighter>
